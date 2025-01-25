b0dfb26870e9d0f620702133579c09df41d50ca5
heisendev13 <madhanklt1331@gmail.com>
2025-01-25 22:28:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header'
+import Main from './components/main'
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
