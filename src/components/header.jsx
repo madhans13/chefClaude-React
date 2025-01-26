b86b9d36d7fdb6d42a02cec242b57219b9d03924
heisendev13 <madhanklt1331@gmail.com>
2025-01-26 22:59:21 +0530

    Add files via upload

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/chef-claude-icon.png"
+export default function Header(){
+    return(
+    <div className="Header">
+    <img src={logo}/>
+    <h1>Chef Claude</h1>
+    </div>
+    )
+}
